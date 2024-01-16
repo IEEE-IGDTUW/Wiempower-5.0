d1c78ca6ef801c8dd9411e486779b90f0d53503a
Apeksha Verma <apeksha2001verma@gmail.com>
2024-01-16 15:44:43 +0530

    Responsive Navbar with hamburger menu

--- script.js
+++ script.js
@@ -9,4 +9,36 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
+function toggleNav() {
+    var navbar = document.querySelector('.navbar');
+    var lineBtn = document.querySelector('#line-btn');
+    var closeBtn = document.querySelector('#close-btn');
+  
+    navbar.classList.toggle('active');
+  
+    if (navbar.classList.contains('active')) {
+      lineBtn.style.display = 'none';
+      closeBtn.style.display = 'block';
+    } else {
+      lineBtn.style.display = 'block';
+      closeBtn.style.display = 'none';
+    }
+  }
+  
+  function handleResize() {
+    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var lineBtn = document.querySelector('#line-btn');
+    var closeBtn = document.querySelector('#close-btn');
+  
+    if (screenWidth <= 830) {
+      lineBtn.style.display = 'block';
+      closeBtn.style.display = 'none';
+    } else {
+      lineBtn.style.display = 'none';
+      closeBtn.style.display = 'none';
+    }
+  }
+  
+  // Call the handleResize function on page load and window resize
+  window.addEventListener('load', handleResize);
+  window.addEventListener('resize', handleResize);
